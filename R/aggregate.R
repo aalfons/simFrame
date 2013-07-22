cd79d9446bf3cc57873dbc938a8fce5ac75a7de8
Andreas Alfons <alfons@ese.eur.nl>
2013-07-22 15:48:16 +0200

    updated aggregate method

--- R/aggregate.R
+++ R/aggregate.R
@@ -6,16 +6,12 @@
 setMethod(
   "aggregate", "SimResults", 
   function(x, select = NULL, FUN = mean, ...) {
-    by <- c(if(length(getEpsilon(x))) "Epsilon", 
-            if(nrow(getTuning(x, which = "cont"))) "ContTuning", 
-            if(length(getNARate(x))) "NARate", 
-            getDesign(x))
-    if(is.null(select)) select <- getColnames(x)
-    else {
-      if(!is.character(select)) stop("'select' must be a character vector")
-      if(!all(select %in% getColnames(x))) stop("undefined columns selected")
-    }
+    # initializations
+    cn <- getColnames(x)
     x <- getValues(x)
+    by <- setdiff(names(x), c("Run", "Rep", "Sample", cn))
+    select <- if(is.null(select)) cn else getCharacter(select, cn)
+    # aggregate simulation results
     if(length(by)) {
       aggregate(x[, select, drop=FALSE], by=x[, by, drop=FALSE], FUN=FUN, ...)
     } else apply(x[, select, drop=FALSE], 2, FUN=FUN, ...)
